2c0e92e2973c9ce50297c488ee043d315e4aa564
shunsuke <shunsuke.nakayama36@gmail.com>
2020-04-09 18:26:33 +0900

    finish3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -9,9 +9,18 @@ class TodolistsController < ApplicationController
 
   	   list.save
 
-  	   redirect_to '/top'
+  	   redirect_to todolist_path(list.id)
   	
   end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
   private
 
   def list_params
