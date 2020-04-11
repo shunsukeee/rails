6433a1f972742df5e0b65f3c1f91688945f85522
shunsuke <shunsuke.nakayama36@gmail.com>
2020-04-11 19:13:10 +0900

    finish3-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fd1d9dd88e2c7c79193124cc2c08c1e1dad9ffa5ff12af3e7004c30ee55a4a90eac3b245a089d342373b4e91d2481089c747e659af9c5de130cc6b4e26457013'
